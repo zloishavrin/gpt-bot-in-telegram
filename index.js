32f6804d125bf04bf8c1c35f6e35c304549821fe
zloishavrin <nexxxusoffical@gmail.com>
2023-03-10 20:12:48 +0300

    Error handler

--- index.js
+++ index.js
@@ -15,13 +15,27 @@ const gpt = new OpenAIApi(configuration);
 //Initializing the TelegramBot Api
 const bot = new TelegramBot(process.env.API_KEY_BOT, {polling: true});
 
+let quantity = 0;
 
 bot.on('message', async msg => {
 
+    quantity = quantity + 1;
+    console.log(`Запрос ${quantity}: ${msg.text}`);
+
+    /*if(msg.text.length > 201) {
+
+        bot.sendMessage(msg.chat.id, `Меньше 200 символов вводи, братан`);
+        console.log(`Запрос ${quantity} завершен!`);
+
+    }*/
     if(msg.text === '/start') {
 
-        await bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}!\n\nThis bot is designed to generate query responses using the Da Vinci 3 model.\n\nJust send your request as a message and wait for a response!`);
+        await bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}!`);
+        await bot.sendMessage(msg.chat.id, `This bot is designed to generate query responses using the Da Vinci 3 model.\n\nJust send your request as a message and wait for a response!`);
+        await bot.sendMessage(msg.chat.id, `Try our VPN service - https://play.google.com/store/apps/details?id=com.shaligulacartel.vpn_shaligula&hl=ru&gl=US`);
         await bot.sendSticker(msg.chat.id, process.env.WELCOME_STICKER_HREF)
+
+        console.log(`Запрос ${quantity} завершен!`);
 
     }
     else {
@@ -34,19 +48,33 @@ bot.on('message', async msg => {
 
         });
 
-        const completion = await gpt.createCompletion({
+        try {
 
-            model: "text-davinci-003",
-            prompt: msg.text,
-            max_tokens: 2000
+            const completion = await gpt.createCompletion({
 
-        });
+                model: "text-davinci-003",
+                prompt: msg.text,
+                max_tokens: 1200
 
-        await bot.sendMessage(msg.chat.id, completion.data.choices[0].text);
-        await bot.deleteMessage(waitMessage.chat.id, waitMessage.message_id);
+            });
+
+            await bot.sendMessage(msg.chat.id, completion.data.choices[0].text);
+            await bot.deleteMessage(waitMessage.chat.id, waitMessage.message_id);
+
+            console.log(`Запрос ${quantity}: Завершен!`);
+
+        }
+        catch(error) {
+
+            await bot.sendMessage(msg.chat.id, 'Попробуйте другой запрос 😊');
+            await bot.deleteMessage(waitMessage.chat.id, waitMessage.message_id);
+
+            console.log(`Запрос ${quantity}: Не завершен!`);
+
+        }
 
     }
 
 })
 
-bot.on("polling_error", (err) => console.log(err));
+bot.on("polling_error", (err) => console.log(err.data.error.message));
